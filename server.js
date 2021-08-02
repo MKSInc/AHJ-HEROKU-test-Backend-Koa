524e0c433676fcb81ee6a7fae9886f375f592ed8
Maksim <pyshkov.maksim@gmail.com>
2021-08-02 22:04:50 +0300

    test: koa-cors

---
+++ server.js
@@ -0,0 +1,66 @@
+const Koa = require('koa');
+const koaCORS = require('koa-cors');
+const app = new Koa();
+
+app.use(koaCORS());
+/*
+app.use(async (ctx, next) => {
+   ctx.response.set({
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'PUT',
+   });
+   await next();
+});*/
+
+app.use((ctx) => {
+   console.log('test');
+   ctx.response.body = 'test';
+});
+
+/*
+app.use(async (ctx, next) => {
+   const origin = ctx.request.get('Origin');
+   console.log(`--- ${ctx.url} ---`);
+   console.log('origin: ', origin);
+   if (origin) {
+      console.log('origin true');
+   } else {
+      console.log('origin false');
+   }
+
+   console.log('method: ', ctx.request.method);
+   await next();
+});
+
+app.use(async (ctx, next) => {
+   console.log('start 1');
+   await next();
+   console.log('continue 1');
+   const rt = ctx.response.get('X-Response-Time');
+   console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+   console.log('end 1');
+});
+
+app.use(async (ctx, next) => {
+   console.log('start 2');
+   const start = Date.now();
+   await next();
+   console.log('continue 2');
+   const ms = Date.now() - start;
+   ctx.set('X-Response-Time', `${ms}ms`);
+   ctx.set('Access-Control-Allow-Origin', '*');
+   ctx.set('Access-Control-Allow-Methods', 'PUT');
+   console.log('end 2');
+});
+
+app.use(async (ctx) => {
+   console.log('start 3');
+   ctx.body = 'Hello world!';
+   console.log('end 3');
+});
+*/
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+   console.log(`Koa server has been started on port ${PORT} ...`);
+});
